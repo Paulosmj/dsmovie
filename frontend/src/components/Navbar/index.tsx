562a641f86d5f242286c3f109878bc3ebf4449b1
Paulosmj <psmeneguesso@gmail.com>
2022-05-11 18:50:00 -0300

    Security config

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -10,7 +10,7 @@ function Navbar() {
           <a href="https://github.com/Paulosmj">
             <div className="dsmovie-contact-container">
               <GithubIcon />
-              <p className="dsmovie-contact-link">/devsuperior</p>
+              <p className="dsmovie-contact-link">/Paulosmj</p>
             </div>
           </a>
         </div>
